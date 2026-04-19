ce836475e8d4b787de3a8e93a7404fe0106cd335
vbmveliz <kill3rian@gmail.com>
2026-04-18 19:53:13 -0500

    feat: agregar 10 nuevas peliculas al sistema

--- SistemaCine/src/procesos/CineService.java
+++ SistemaCine/src/procesos/CineService.java
@@ -7,10 +7,14 @@ public class CineService {
 
     private ArrayList<Pelicula> peliculas = new ArrayList<>();
 
+    
+    //METODOS CAMELCASE
+    
+    // Añadir pelicula 
     public void registrarPelicula(Pelicula p) {
         peliculas.add(p);
     }
-
+ // Listar pelicula
     public void listarPeliculas() {
         for (Pelicula p : peliculas) {
             System.out.println(p);
